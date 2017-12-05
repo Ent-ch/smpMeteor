6d6682f11d3ccdd91519f272549041e7034a4014
Vladimir Ryabchich <vladimir.ryabchich@gmail.com>
2017-12-05 15:13:17 +0200

    detail info

--- imports/ui/FormsData.jsx
+++ imports/ui/FormsData.jsx
@@ -7,6 +7,9 @@ import {
     TableRow,
     TableRowColumn,
 } from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 import { withTracker } from 'meteor/react-meteor-data';
 import { userForms } from '../api/userForms';
 
@@ -16,32 +19,71 @@ class FormsData extends Component {
 
         this.state = {
             hideCompleted: false,
+            open: false,
+            seletedElement: null
         };
+    }
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    handleClick = event => {
+        console.log(event);
+        this.setState({ seletedElement: event, open: true });
+    }
+
+
+    renderDialog() {
+        const element = this.props.userForms[this.state.seletedElement];
+        const actions = [
+            <FlatButton
+                label="Ok"
+                primary={true}
+                onClick={this.handleClose}
+            />
+        ];
+
+        return (
+            <Dialog
+                actions={actions}
+                modal={false}
+                open={this.state.open}
+                onRequestClose={this.handleClose}
+            >
+                <div>
+                    {JSON.stringify(element)}
+                </div>
+            </Dialog>
+        );
     }
 
     render() {
         const { userForms } = this.props;
         
-        return <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHeaderColumn>Name</TableHeaderColumn>
-                    <TableHeaderColumn>Phone</TableHeaderColumn>
-                    <TableHeaderColumn>E-mail</TableHeaderColumn>
-                    <TableHeaderColumn>Address</TableHeaderColumn>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {userForms.map((el, i) => 
-                    <TableRow key={i}>
-                        <TableRowColumn>{el.name + (el.firstName || ' ')}</TableRowColumn>
-                        <TableRowColumn>{el.phone}</TableRowColumn>
-                        <TableRowColumn>{el.email}</TableRowColumn>
-                        <TableRowColumn>{el.Address ? `${el.Address.Detail}, ${el.Address.City}, ${el.Address.Province}, ${el.Address.postalCode} , ${el.Address.Country}` : ''}</TableRowColumn>
+        return <div>
+            {this.renderDialog()}
+                <Table onCellClick={this.handleClick.bind(this)}>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderColumn>Name</TableHeaderColumn>
+                        <TableHeaderColumn>Phone</TableHeaderColumn>
+                        <TableHeaderColumn>E-mail</TableHeaderColumn>
+                        <TableHeaderColumn>Address</TableHeaderColumn>
                     </TableRow>
-                )}
-            </TableBody>
-        </Table>;
+                </TableHeader>
+                <TableBody>
+                    {userForms.map((el, i) => 
+                        <TableRow key={i}>
+                            <TableRowColumn>{el.name + (el.firstName || ' ')}</TableRowColumn>
+                            <TableRowColumn>{el.phone}</TableRowColumn>
+                            <TableRowColumn>{el.email}</TableRowColumn>
+                            <TableRowColumn>{el.Address ? `${el.Address.Detail}, ${el.Address.City}, ${el.Address.Province}, ${el.Address.postalCode} , ${el.Address.Country}` : ''}</TableRowColumn>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>;
     }
 };
 
